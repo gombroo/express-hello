f0cfd80e0800eadffe9e8e60de891b37d204f705
Ola Siem <info@siemieniuk.net>
2019-12-20 15:27:56 +0100

    Init project

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname + `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use('/user', (req, res, next) => {
+  res.send('Please log in');
+  next();
+});
+
+app.get('/user/panel', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/forbidden.html'));
+});
+
+app.get('/user/settings', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/forbidden.html'));
+});
+
+
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname + `/views/404.html`));
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
