aa860126ef45aea774a25ab8930889b8605d85b8
Ola Siem <info@siemieniuk.net>
2019-12-23 16:47:41 +0100

    Add valitation alerts and basic css styles

--- server.js
+++ server.js
@@ -63,10 +63,12 @@ app.post('/contact/send-message', (req, res) => {
   const { author, sender, title, message } = req.body;
 
   if(author && sender && title && message) {
-    res.send('The message has been sent!');
+    // res.send('The message has been sent!');
+    res.render('contact', { isSent: true });
   }
   else {
-    res.send('You can\'t leave fields empty!')
+    // res.send('You can\'t leave fields empty!')
+    res.render('contact', { isError: true });
   }
 });
 
